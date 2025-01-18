d46a584d0e29cdac7ab7391bf0adb7fdab8b43fc
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-01-18 15:52:31 +0100

    shorten include path only for windows
    since it hits compile performance

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -27,6 +27,7 @@ import subprocess
 import json
 import semantic_version
 import os
+import sys
 import shutil
 from os.path import join
 
@@ -45,6 +46,7 @@ mcu = board.get("build.mcu", "esp32")
 board_sdkconfig = board.get("espidf.custom_sdkconfig", "")
 entry_custom_sdkconfig = "\n"
 flag_custom_sdkconfig = False
+IS_WINDOWS = sys.platform.startswith("win")
 
 if config.has_option("env:"+env["PIOENV"], "custom_sdkconfig"):
     entry_custom_sdkconfig = env.GetProjectOption("custom_sdkconfig")
@@ -256,7 +258,8 @@ if flag_custom_sdkconfig == True and flag_any_custom_sdkconfig == False:
     call_compile_libs()
 
 if "arduino" in env.subst("$PIOFRAMEWORK") and "espidf" not in env.subst("$PIOFRAMEWORK") and env.subst("$ARDUINO_LIB_COMPILE_FLAG") in ("Inactive", "True"):
-    env.AddBuildMiddleware(shorthen_includes)
+    if IS_WINDOWS:
+        env.AddBuildMiddleware(shorthen_includes)
     if os.path.exists(join(FRAMEWORK_DIR, "tools", "platformio-build.py")):
         PIO_BUILD = "platformio-build.py"
     else:
